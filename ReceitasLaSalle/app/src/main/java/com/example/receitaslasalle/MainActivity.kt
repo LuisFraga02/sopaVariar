e2675ec9a4696d8855843de85e9ae3dd75fb2fca
Thiago Goulart <thiaggoulart@gmail.com>
2024-11-29 13:53:59 -0300

    adiciona login

--- ReceitasLaSalle/app/src/main/java/com/example/receitaslasalle/MainActivity.kt
+++ ReceitasLaSalle/app/src/main/java/com/example/receitaslasalle/MainActivity.kt
@@ -2,21 +2,32 @@ package com.example.receitaslasalle
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG : String = "FIREBASE"
+    }
 
     lateinit var user: EditText
     lateinit var pass: EditText
     lateinit var loginButton: Button
     lateinit var cadastroButton: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
 
         user = findViewById(R.id.userid)
         pass = findViewById(R.id.pass)
@@ -30,15 +41,46 @@ class MainActivity : AppCompatActivity() {
 
 
         loginButton.setOnClickListener {
-            var intent = Intent(applicationContext, Tela2::class.java)
-            startActivity(intent)
-//            if (user.text.toString() != "qualquercoisaqualquercoisa" &&
-//                pass.text.toString() != "qualquercoisaqualquercoisa"){
-//
-//                var intent = Intent(applicationContext, Tela2::class.java)
-//                startActivity(intent)
-//            }
+            if (user.text.toString() != "qualquercoisaqualquercoisa" &&
+                pass.text.toString() != "qualquercoisaqualquercoisa"){
+
+
+            }
         }
 
+    }
+
+    fun loginFireabase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    goToList()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    fun goToList() {
+        val intent = Intent(applicationContext, Tela2::class.java)
+        startActivity(intent)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            goToList()
+        }
     }
 }
--- ReceitasLaSalle/app/src/main/java/com/example/receitaslasalle/cadastro.kt
+++ ReceitasLaSalle/app/src/main/java/com/example/receitaslasalle/cadastro.kt
@@ -2,17 +2,27 @@ package com.example.receitaslasalle
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.receitaslasalle.databinding.ActivityCadastroBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class cadastro : AppCompatActivity() {
+    companion object {
+        private const val TAG : String = "FIREBASE"
+    }
+
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding : ActivityCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
@@ -23,8 +33,35 @@ class cadastro : AppCompatActivity() {
         }
 
         binding.CadastrarButton.setOnClickListener{
-            Toast.makeText(this, binding.CadastrarNome.text.toString(), Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, binding.CadastrarPass.text.toString(), Toast.LENGTH_SHORT).show()
+            val email = binding.CadastrarNome.text.toString()
+            val pass = binding.CadastrarPass.text.toString()
+            createAccount(email, pass)
         }
+    }
+
+    fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    goToMain()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+//                    updateUI(null)
+                }
+            }
+    }
+
+    fun goToMain() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
